974aedb9a51e3953f0848291d7e6ef9a481efb43
balakrish181 <balakrish181@gmail.com>
2024-08-09 20:37:40 +0100

    reduced complexity

--- ddpm_torchrun/model.py
+++ ddpm_torchrun/model.py
@@ -1,4 +1,3 @@
-
 from config import TrainingConfig
 from diffusers import UNet2DModel
 
@@ -18,28 +17,21 @@ def unet_model(config):
         sample_size=config.image_size,  # The target image resolution.
         in_channels=3,  # Number of input channels; 3 for RGB images.
         out_channels=3,  # Number of output channels.
-        layers_per_block=2,  # Number of ResNet layers per UNet block.
-        block_out_channels=(128, 128, 256, 256, 512, 512, 1024),  # Output channels for each UNet block.
+        layers_per_block=1,  # Number of ResNet layers per UNet block.
+        block_out_channels=(32, 64, 64),  # Output channels for each UNet block.
         down_block_types=(
             "DownBlock2D",  # A regular ResNet downsampling block.
-            "DownBlock2D",
-            "DownBlock2D",
-            "DownBlock2D",
-            "AttnDownBlock2D",  # A ResNet downsampling block with spatial self-attention.
-            "AttnDownBlock2D",
-            "DownBlock2D",
+            "DownBlock2D",  # A regular ResNet downsampling block.
+            "DownBlock2D",  # A regular ResNet downsampling block.
         ),
         up_block_types=(
             "UpBlock2D",  # A regular ResNet upsampling block.
-            "AttnUpBlock2D",  # A ResNet upsampling block with spatial self-attention.
-            "AttnUpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
+            "UpBlock2D",  # A regular ResNet upsampling block.
+            "UpBlock2D",  # A regular ResNet upsampling block.
         ),
     )
     return model
 
-if __name__=='__main__':
+if __name__ == '__main__':
     model = unet_model(config)
+    # You can add additional code here to test the model or print its architecture.
