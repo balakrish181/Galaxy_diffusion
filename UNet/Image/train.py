1e7e38340d4654178c0fa41d34022fa4e807bf31
balakrish181 <balakrish181@gmail.com>
2024-08-24 09:36:55 +0100

    ported denoising_diffusion_pytorch

---
+++ UNet/Image/train.py
@@ -0,0 +1,52 @@
+
+from PIL import Image
+from pathlib import Path
+import torch 
+from torchvision import transforms
+
+from denoising_diffusion_pytorch.denoising_diffusion_pytorch import Unet,GaussianDiffusion,Trainer
+
+
+paths = Path('../images2.0/')
+print(paths.exists())
+
+image_paths = list(Path(paths).glob('*.png'))
+
+
+
+def main():
+    model = Unet(
+    32,
+    dim_mults= (1,2,4,4,8),
+    flash_attn=True
+)
+    print(f'Number of parameters in the model is {sum(i.numel() for i in model.parameters())}')
+
+
+    diffusion = GaussianDiffusion(
+        model,
+        image_size=512,
+        timesteps=1000,
+        sampling_timesteps=250
+    )
+
+    trainer = Trainer(
+        diffusion,
+        paths,
+        train_batch_size = 4,
+        train_lr = 5e-4,
+        train_num_steps = 60000,         # total training steps
+        gradient_accumulate_every = 5,    # gradient accumulation steps
+        ema_decay = 0.995,                # exponential moving average decay
+        amp = True,                       # turn on mixed precision
+        calculate_fid = False,              # whether to calculate fid during training
+        save_and_sample_every = 2000,
+        num_samples = 4,
+        results_folder = './results',
+    
+    )
+    #trainer.load(3)
+    trainer.train()
+
+if __name__ =='__main__':
+    main()
